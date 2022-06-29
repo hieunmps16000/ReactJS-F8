98d92ea8a051d2244a0ace1cfb20ddc28bb4530f
Hieu Nguyen <lotous12a4@gmail.com>
2022-06-29 19:23:59 +0700

    React with Type(string/function/class)

--- app.js
+++ app.js
@@ -1,80 +1,16 @@
-const root = document.getElementById('root')
-/**
- * Task 1: 
- * >>>
- *  <div class="post-item">
- *      <h2 title="Học React tại F8">Học ReactJS</h2>
- *      <p>Học ReactJS từ cơ bản đến nâng cao</p>
- *  </div
- */
-
-let info = React.createElement(
-    'div',
-    {
-        className: 'post-item',
-        style: {
-            color: 'white',
-            fontSize: '20px',
-            backgroundColor: '#000',
-            padding: '20px',
-            borderRadius: '14px',
-            textAign: 'center',
-            boxShadow: '0 0 40px rgba(255, 255, 255, 0.6)'
-        }
-    },
-    React.createElement(
-        'h2',
-        {
-            title: 'HỌc React tại F8'
-        },
-        'Học ReactJS'
-    ),
-    React.createElement(
-        'p',
-        {},
-        'Học ReactJS từ cơ bản đến nâng cao'
+function PostItem() {
+    return (
+        <div className="post-item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Qvov3Fy5FKt44_Y-rynYY8liqZ4YRg9jrg&usqp=CAU" alt="" />
+            <h2>Học ReactJS tại F8</h2>
+        </div>
     )
+}
+
+const app = (
+    <div className="posts-list">
+        <PostItem />
+    </div>
 )
 
-// Don't use React.createElement()
-// New way
-let newDescription = '@Hieunmps2022'
-let spanStyle = {
-    color: '#fff',
-    fontStyle: 'italic',
-    fontSize: '12px'
-}
-let info2 = 
-    <div className="Học React tại F8">
-        <h2 title="Học React tại F8">Học ReactJS</h2>
-        <p>Học ReactJS từ cơ bản đến nâng cao</p>
-        <span style={spanStyle}>{newDescription}</span>
-    </div>
-
-// Homework:
-const courses = [
-    {
-        id: 1,
-        name: 'HTML-CSS',
-        coins: 100
-    },
-    {
-        id: 2,
-        name: 'Javascript',
-        coins: 200
-    },
-    {
-        id: 3,
-        name: 'Angular',
-        coins: 250
-    }
-];
-
-const listItems = courses.map(course => {
-    return <li>{course.name}</li>
-})
-
-const ul = <ul>
-    {listItems}
-</ul>
-ReactDOM.render(ul, root)
+ReactDOM.render(app, document.getElementById('root'))
